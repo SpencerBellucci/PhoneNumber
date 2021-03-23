09f39390144b9fa6f67495a25ace487d49bcbf59
SpencerBellucci <spencer.bellucci@mymail.champlain.edu>
2021-03-22 22:08:13 -0400

    finished project

--- Sources/PhoneNumberMnemonics/WordList.swift
+++ Sources/PhoneNumberMnemonics/WordList.swift
@@ -6,3 +6,22 @@ import Foundation
 // YOU FILL IN HERE
 // You'll want to create a mechanism here for loading and querying
 // words.txt. It's up to you how you do this. You may consider a struct.
+
+// struct for file i/o
+struct fileIO {
+    // url of file
+    let fileURL = "/Users/spencerbellucci/Desktop/EmergingLang/PhoneNumber/words.txt"
+    
+    // func to put contents of file in a string
+    func readFile() -> String {
+        do {
+            let contents = try String(contentsOfFile: fileURL)
+            return contents.uppercased()
+        } catch { return "Error" }
+    }
+    // check for words in list with min length
+    func querryFile(message: String, list: [String], length: UInt) -> [String] {
+        let array = list.map { message.contains($0) ? $0 : ""}.filter { $0 != "" && $0.count >= length}
+        return array
+    }
+}
--- Sources/PhoneNumberMnemonics/WordSearch.swift
+++ Sources/PhoneNumberMnemonics/WordSearch.swift
@@ -3,7 +3,17 @@
 
 import Foundation
 
-// YOU FILL IN HERE
+let zero = ["0"]
+let one = ["1"]
+let two = ["A", "B", "C"]
+let three = ["D", "E", "F"]
+let four = ["G", "H", "I"]
+let five = ["J", "K", "L"]
+let six = ["M", "N", "O"]
+let seven = ["P", "Q", "R", "S"]
+let eight = ["T", "U", "V"]
+let nine = ["W", "X", "Y", "Z"]
+
 // Feel free to put in additional utility code as long as you have
 // no loops, no *mutable* global variables, and no custom classes.
 
@@ -11,7 +21,46 @@ import Foundation
 // possible letters that could be in place of that character
 // For instance, 234 becomes [["A", "B", "C"], ["D", "E", "F"], ["G", "H", "I"]]
 public func letters(for phoneNumber: String) -> [[String]] {
-    // YOU FILL IN HERE
+    // hold the letters
+    var letterArray: [[String]] = []
+    
+    // define recursive function for converting numbers to letters
+    func printNumber(num: String, offset: Int, maxOffset: Int) {
+        // if phone number length has not been reached
+        if (offset != maxOffset) {
+            // index the next number
+            let i = num[num.index(num.startIndex, offsetBy: offset)]
+            // find which letters to add
+            switch(i) {
+                case "0":
+                    letterArray.append(zero)
+                case "1":
+                    letterArray.append(one)
+                case "2":
+                    letterArray.append(two)
+                case "3":
+                    letterArray.append(three)
+                case "4":
+                    letterArray.append(four)
+                case "5":
+                    letterArray.append(five)
+                case "6":
+                    letterArray.append(six)
+                case "7":
+                    letterArray.append(seven)
+                case "8":
+                    letterArray.append(eight)
+                case "9":
+                    letterArray.append(nine)
+                default:
+                    print("Error")
+            }
+            printNumber(num: num, offset: offset + 1, maxOffset: maxOffset)
+        }
+    }
+    // call recursive function
+    printNumber(num: phoneNumber, offset: 0, maxOffset: 10)
+    return letterArray
 }
 
 // Finds all of the ordered permutations of a given
@@ -21,27 +70,60 @@ public func letters(for phoneNumber: String) -> [[String]] {
 // For instance permuations(of: [["a", "b"], ["c"], ["d", "e"]]) will return
 // ["acd", "ace" "bcd", "bce"]
 public func permutations(of arrays: [[String]]) -> [String] {
-    // YOU FILL IN HERE
+    // get the first array in arrays
+    let arraysFirst = arrays[0]
+    
+    // use high-order functions to find permutations
+    let perm = arrays.dropFirst().reduce(arraysFirst){( first, word ) in
+        first.flatMap {( i ) in
+            word.map {
+                j in i + j
+            }
+        }
+    }
+    // return the array
+    return perm
 }
 
 // Finds all of the possible strings of characters that a phone number
 // can potentially represent
 // Uses letters(for:) and permutations(of:) to do this
 public func possibles(for phoneNumber: String) -> [String] {
-    // YOU FILL IN HERE
+    // find possible letters
+    let possibleLetters = letters(for: phoneNumber)
+    // find permutations of said letters
+    let perms = permutations(of: possibleLetters)
+    
+    return perms
 }
 
 // Returns all of the words in a given *string* from the wordlist.txt file
 // using only words in the word list of minimum length ofMinLength
 public func wordsInString(_ string: String, ofMinLength length: UInt) -> [String] {
-    // YOU FILL IN HERE
+    // create the file object
+    let file = fileIO()
+
+    // querry the file, then turn string into array
+    let text = (file.readFile()).components(separatedBy: "\n").filter { $0 != "" }
+
+    // find all the words that occur in the message
+    let words = file.querryFile(message: string, list: text, length: length)
+
+    return words
 }
 
 // Returns all possibles strings of characters that a phone number
 // can potentially represent that contain words in words.txt
 // greater than or equal to ofMinLength characters
 public func possiblesWithWholeWords(ofMinLength length: UInt, for phoneNumber: String) -> [String] {
-    // YOU FILL IN HERE
+    // possible letter combos
+    let letterCombo = possibles(for: phoneNumber)
+    // find which of these has a word from words.txt in the combo
+    let posWords = letterCombo.filter {
+        wordsInString($0, ofMinLength: length).count > 0
+    }
+    
+    return posWords
 }
 
 // Returns the phone number mnemonics that have the most words present in words.txt
@@ -50,11 +132,37 @@ public func possiblesWithWholeWords(ofMinLength length: UInt, for phoneNumber: S
 // words.txt, it will return both of them, if the are no other mnemonics
 // that contain more than three words
 public func mostWords(for phoneNumber: String) -> [String] {
-    // YOU FILL IN HERE
+    // get possible
+    let letterCombo = possibles(for: phoneNumber)
+
+    // find words from words.txt present in possible
+    let words = letterCombo.map { i in
+        return wordsInString(i, ofMinLength: 0)
+    }.filter { $0 != [] }
+
+    // find that ones with most words present in one possible
+    if let max = words.max(by: {$1.count > $0.count}) {
+        return(max)
+    }
+    return[""]
 }
 
 // Returns the phone number mnemonics with the longest words from words.txt
 // If more than one word is tied for the longest, returns all of them
 public func longestWords(for phoneNumber: String) -> [String] {
-    // YOU FILL IN HERE
+    // get possible
+    let letterCombo = possibles(for: phoneNumber)
+
+    // find words from words.txt present in possible, and filter into one array
+    let words = letterCombo.map { i in
+        return wordsInString(i, ofMinLength: 0)
+    }.filter { $0 != [] }.reduce([]) { array, words in
+        array + words
+    }
+
+    // find the longest string in array
+    if let longest = words.max(by: {$1.count > $0.count}) {
+        return [longest]
+    }
+    return [""]
 }
